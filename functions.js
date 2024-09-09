25d2d4d61d5c10fc1027b5b6cc27f610bd6750b8
Manzana475 <156945307+Manzana475@users.noreply.github.com>
2024-09-08 20:50:43 -0500

    Project 1, Iteration 3 (Completed)

--- functions.js
+++ functions.js
@@ -1,24 +1,30 @@
+function load_style()
+{
+   page_style = localStorage.getItem('page_stylesheet_name');
+  if (page_style === null)
+  {
+    page_style = "style.css";
+  }
+  document.getElementById('page_style').setAttribute("href", page_style);
+}
+
+
 function switchStyle() {
     var stylesheet = document.getElementById('page_style');
 
-    if (stylesheet.getAttribute('href') == 'style.css') {
+    if (stylesheet.getAttribute('href') == 'style.css') { //Switching to style2
+      localStorage.setItem("page_stylesheet_name", "style2.css");
       stylesheet.setAttribute('href', 'style2.css');
       document.ElementsByClassName('border-container').setAttribute('href', 'background-rectangle');
       load_style();
     } 
-    else {
+    else { //Switching to style1
+      localStorage.setItem("page_stylesheet_name", "style.css");
       stylesheet.setAttribute('href', 'style.css');
+      document.ElementsByClassName('background-rectnagle').setAttribute('href', 'border-container');
       load_style();
     }
   }
 
-  function load_style()
-  {
-     page_style = localStorage.getItem('page_style');
-    if (page_style === null)
-    {
-    page_style = "style.css";
-    }
-    document.getElementById('page_style').setAttribute("href", page_style);
-  }
+  load_style();
 
